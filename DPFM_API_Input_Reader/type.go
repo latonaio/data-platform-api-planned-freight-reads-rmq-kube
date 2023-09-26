62b61bdf1f73cadd13eb06011fe7a62726511509
Arisa Maeyama <Arisa Maeyama>
2023-09-26 15:03:15 +0900

    release

--- DPFM_API_Input_Reader/type.go
+++ DPFM_API_Input_Reader/type.go
@@ -121,6 +121,17 @@ type Header struct {
 	Item                                    []Item   `json:"Item"`
 }
 
+type HeaderDoc struct {
+	PlannedFreight           int     `json:"PlannedFreight"`
+	DocType                  string  `json:"DocType"`
+	DocVersionID             int     `json:"DocVersionID"`
+	DocID                    string  `json:"DocID"`
+	FileExtension            *string `json:"FileExtension"`
+	FileName                 *string `json:"FileName"`
+	FilePath                 *string `json:"FilePath"`
+	DocIssuerBusinessPartner *int    `json:"DocIssuerBusinessPartner"`
+}
+
 type Item struct {
 	FreightAgreement                        int      `json:"FreightAgreement"`
 	FreightAgreementItem                    int      `json:"FreightAgreementItem"`
